b7053d9b60e086a88f8d14ed060ef6dbfc636129
wtaype <wilder.taype16@gmail.com>
2025-05-16 21:46:58 -0500

    Actualizacion de archivos

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
-import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [ghPages()],
-  base: '/witimex/', // Reemplaza con el nombre de tu repositorio
+  base: '/witimex/', // Asegúrate de que el nombre del repositorio sea correcto
 });
