fd1cfdd387784b87b16d4ffaab50e51c909b0d05
wtaype <wilder.taype16@gmail.com>
2025-05-17 00:16:10 -0500

    Actualizando versiones

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ export default defineConfig(({ mode }) => {
   const base = mode === 'v20' ? '/witimex/v20/' : '/witimex/';
   return {
     base,
+    build: {
+      rollupOptions: {
+        input: 'index.html', // Asegúrate de que siempre use el archivo de entrada correcto
+      },
+    },
   };
 });
